1b9a4d418f1621f44be22b1bf8a3b1c4744ca30b
Amany Ashraf <amany.ashraf12010@gmail.com>
2026-07-25 14:42:17 -0700

    Update footer social media links and add TikTok icon

--- components/layout/footer/Footer.tsx
+++ components/layout/footer/Footer.tsx
@@ -1,6 +1,6 @@
 import Container from "@/components/shared/Container";
 import Link from "next/link";
-import { FaFacebook, FaInstagram, FaYoutube, FaTelegram } from "react-icons/fa";
+import { FaFacebook, FaInstagram, FaYoutube, FaTiktok } from "react-icons/fa";
 
 export default function Footer() {
   return (
@@ -29,19 +29,43 @@ export default function Footer() {
 
             <div className="flex gap-4 justify-center">
 
-              <a className="p-3 rounded-xl bg-white/10 hover:bg-[#7D79F1] transition">
-                <FaTelegram size={18} />
+              <a
+                href="https://www.tiktok.com/@aplus.edu?_r=1&_t=ZS-98KJugN2G4z"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="p-3 rounded-xl bg-white/10 hover:bg-[#7D79F1] transition cursor-pointer"
+                title="تيك توك"
+              >
+                <FaTiktok size={18} />
               </a>
 
-              <a className="p-3 rounded-xl bg-white/10 hover:bg-[#7D79F1] transition">
+              <a
+                href="https://youtube.com/@a-aplus-edu?si=hezp-sFj6D6PHca2"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="p-3 rounded-xl bg-white/10 hover:bg-[#7D79F1] transition cursor-pointer"
+                title="يوتيوب"
+              >
                 <FaYoutube size={18} />
               </a>
 
-              <a className="p-3 rounded-xl bg-white/10 hover:bg-[#7D79F1] transition">
+              <a
+                href="https://www.instagram.com/aplus.edu1?igsh=OGJtZXUxazk2cDlu"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="p-3 rounded-xl bg-white/10 hover:bg-[#7D79F1] transition cursor-pointer"
+                title="انستجرام"
+              >
                 <FaInstagram size={18} />
               </a>
 
-              <a className="p-3 rounded-xl bg-white/10 hover:bg-[#7D79F1] transition">
+              <a
+                href="https://www.facebook.com/share/1CV9tUcGHN/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="p-3 rounded-xl bg-white/10 hover:bg-[#7D79F1] transition cursor-pointer"
+                title="فيسبوك"
+              >
                 <FaFacebook size={18} />
               </a>
 
